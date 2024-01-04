97e378b2013e2762d7f39cff0c1c1ae8519a9753
abel-tefera <abeltefera16@gmail.com>
2024-01-04 13:19:45 +0300

    Header set up with auth

---
+++ src/components/header.tsx
@@ -0,0 +1,75 @@
+import Link from "next/link";
+import {
+  Navbar,
+  NavbarBrand,
+  NavbarContent,
+  NavbarItem,
+  Input,
+  Button,
+  Avatar,
+  Popover,
+  PopoverTrigger,
+  PopoverContent,
+} from "@nextui-org/react";
+import { auth } from "@/auth";
+import { signIn, signOut } from "@/actions";
+
+export default async function Header() {
+  const session = await auth();
+
+  let authContent: React.ReactNode;
+
+  if (session?.user) {
+    authContent = (
+      <Popover placement="left">
+        <PopoverTrigger>
+          <Avatar src={session.user.image || ""} />
+        </PopoverTrigger>
+        <PopoverContent>
+          <div className="p-4">
+            <form action={signOut}>
+              <Button type="submit" color="secondary" variant="ghost">
+                Sign Out
+              </Button>
+            </form>
+          </div>
+        </PopoverContent>
+      </Popover>
+    );
+  } else {
+    authContent = (
+      <>
+        <NavbarItem>
+          <form action={signIn}>
+            <Button type="submit" color="secondary" variant="bordered">
+              Sign In
+            </Button>
+          </form>
+        </NavbarItem>
+        <NavbarItem>
+          <form action={signIn}>
+            <Button type="submit" color="primary" variant="bordered">
+              Sign Up
+            </Button>
+          </form>
+        </NavbarItem>
+      </>
+    );
+  }
+
+  return (
+    <Navbar className="shadow mb-6">
+      <NavbarBrand>
+        <Link href="/" className="font-bold">
+          Discuss
+        </Link>
+      </NavbarBrand>
+      <NavbarContent justify="center">
+        <NavbarItem>
+          <Input placeholder="Search" width="200px" />
+        </NavbarItem>
+      </NavbarContent>
+      <NavbarContent justify="end">{authContent}</NavbarContent>
+    </Navbar>
+  );
+}
